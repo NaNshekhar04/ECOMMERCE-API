b5526b06eeab5408121b10036d46f1404d3e3ed3
Shekhar.Thakur04 <connectshekharthakur09@gmail.com>
2022-10-14 20:11:51 +0530

    Creating Products Schema

---
+++ models/ProdSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  
+  const Product = mongoose.model("Product", ProductSchema);
+  
+  module.exports = Product;
